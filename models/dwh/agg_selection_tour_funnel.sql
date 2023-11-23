27373c7f556099ec2830693841cc66817f9ecad4
Kyle Sullivan <kyle.sullivan@getyourguide.com>
2023-11-23 14:20:36 +0100

    finish model

--- models/dwh/agg_selection_tour_funnel.sql
+++ models/dwh/agg_selection_tour_funnel.sql
@@ -6,13 +6,19 @@
     'FirstBooking',
     'ThirdReview'
     ] %}
+with funnel_events as (
 
+    select * from {{ source('dbt_poc', 'agg_selection_tour_funnel_events')}}
+
+
+)
 select
 
     tour_id,
-    {% for item in ['TourCreationStarted', 'TourContentAdded', 'TourSubmitted', 'TourApproved', ] %}
+    {% for item in ['tourcreationstarted', 'toursubmitted', 'tourapproved', 'firstbooking'] %}
 
-        min(case when funnel_date)
-
+        min(case when funnel_step = lower({{item}}) then date_id else null end) as date_{{item}}
     {% endfor  %}
+
+   from
 
